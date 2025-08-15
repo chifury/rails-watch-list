0b38dd70dee6a80764617aa43fdf890825c64cdc
Louis Bourne <chifury@proton.me>
2025-08-15 16:16:17 +0900

    photo upload

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -17,6 +17,7 @@ class ListsController < ApplicationController
     @list = List.new(list_params)
     if @list.save
       redirect_to list_path(@list)
+      # raise
     else
       render "new", status: :unprocessable_content
     end
@@ -25,6 +26,6 @@ class ListsController < ApplicationController
   private
 
   def list_params
-    params.require(:list).permit(:name)
+    params.require(:list).permit(:name, :photo)
   end
 end
