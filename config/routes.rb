6d39a45ab9969621b0fed24745b922421b603373
Louis Bourne <chifury@proton.me>
2025-08-19 15:15:53 +0900

    comments deleted

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,4 @@
 Rails.application.routes.draw do
-  # resources :movies do
-  #   resources :bookmarks
-  # end
-
-  # resources :lists do
-  #   resources :bookmarks
-  # end
   root to: "lists#index"
 
   resources :lists, only: [:show, :new, :create] do
